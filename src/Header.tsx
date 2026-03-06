9b05151d02409b2855dc7564c689e4684f93c293
MickaelDutfoy <m.dutfoy@gmail.com>
2026-03-06 08:14:44 +0100

    content update

--- src/Header.tsx
+++ src/Header.tsx
@@ -21,15 +21,15 @@ const Header = ({
       <div className="box short">
         <p>
           {lang === "fr" &&
-            "Développeur TypeScript / Next.js, je recherche un premier poste après 2 mois d’expérience en entreprise et 12 années d’enseignement des mathématiques. Je développe et maintiens Aster, une application Next.js en production pour les associations de protection animale."}
+            "Développeur TypeScript / Next.js, je recherche un premier poste après 2 mois d’expérience en entreprise et 12 années d’enseignement des mathématiques. Persévérant, attentif aux détails et créatif, je développe actuellement Aster, une application Next.js en production pour les associations de protection animale."}
         </p>
         <p>
           {lang === "en" &&
-            "TypeScript / Next.js developer, I am seeking my first full-time position after 2 months of experience in a company and 12 years as a mathematics teacher. I develop and maintain Aster, a Next.js application in production for animal welfare organizations."}
+            "TypeScript / Next.js developer, I am seeking a first full-time role after two months of industry experience and twelve years teaching mathematics. Persistent, detail-oriented and creative, I am currently developing Aster, a production Next.js application for animal welfare organizations."}
         </p>
         <p>
           {lang === "nb" &&
-            "TypeScript / Next.js-utvikler, jeg søker min første faste stilling etter 2 måneders erfaring fra arbeidslivet og 12 års erfaring som matematikklærer. Jeg utvikler og vedlikeholder Aster, en Next.js-applikasjon i produksjon for dyrevernorganisasjoner."}
+            "TypeScript / Next.js-utvikler, jeg søker min første faste stilling etter to måneders erfaring i bransjen og tolv år som matematikklærer. Utholdende, detaljorientert og kreativ utvikler jeg for tiden Aster, en Next.js-applikasjon i produksjon for dyrevernorganisasjoner."}
         </p>
       </div>
       <Language lang={lang} setLang={setLang} />

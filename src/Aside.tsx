ff8ba3ced75f3372a1e71df44a68f21f573dba29
MickaelDutfoy <m.dutfoy@gmail.com>
2026-08-26 19:56:26 +0200

    content update

--- src/Aside.tsx
+++ src/Aside.tsx
@@ -59,7 +59,7 @@ const Aside = ({lang, title}: {lang: string; title: string}) => {
             {lang === 'en' && "Norway"}
             {lang === 'nb' && "Norge"}
             <br/>
-            <a className="aside-link" href="tel:+33682506341">(+33)06.82.50.63.41</a><br/>
+            <a className="aside-link" href="tel:+33682506341">(+47)484.04.203</a><br/>
             <a className="aside-link" href="mailto:m.dutfoy@gmail.com" target="_blank">m.dutfoy@gmail.com</a>
         </address> : <ul className="aside-list">{generateContent(lang, title).content.map(item => <li key={item}>{item}</li>)}</ul>}
       </aside>
